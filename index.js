dba7f9302e47a959f13ddfea82aa8f952b41bd5e
James Sheen <james.sheen@perfectward.com>
2021-06-07 06:28:32 +0100

    slackbot

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const SlackBot = require('slackbots');
-const axios = require('axios')
-
+const chalk = require('chalk');
 const dotenv = require('dotenv')
 const zonedTimeToUtc = require('date-fns-tz/zonedTimeToUtc')
 const format = require('date-fns-tz/format')
+var getDayOfYear = require('date-fns/getDayOfYear')
+var getDay = require('date-fns/getDay')
+var quotes = require('./quotes.json')
 
-const defaultFormat = 'DD/MM/YYYY HH:mm'
+
 const defaultTimezone = 'Europe/London'
+const log = console.log;
 
 const unixToDate = unix => new Date(parseInt(unix, 10) * 1000)
 
@@ -15,11 +18,13 @@ const formatDate = (value, timezone = defaultTimezone) => {
   return zonedTimeToUtc(unixToDate(value), timezone)
 }
 
+const getRandomArrayItem = (arr) => arr[Math.floor(Math.random() * arr.length)]
+
 dotenv.config()
 
 const bot = new SlackBot({
   token: `${process.env.BOT_TOKEN}`,
-  name: 'jim'
+  name: 'pwbot'
 })
 
 // last date it was updated
@@ -27,27 +32,51 @@ let lastUpdated = null
 
 bot.on('start', function() {
   // more information about additional params https://api.slack.com/methods/chat.postMessage
-  var params = {
-    icon_emoji: ':mega:',
-  };
+
 
   function pollDate() {
     const d = zonedTimeToUtc(new Date(), defaultTimezone);
     const h = d.getHours();
     const m = d.getMinutes();
     const s = d.getSeconds();
-    if (s % 10 === 0) {
-      if (lastUpdated !== d) {
+    const dayOfYear = getDayOfYear(d);
+    const dayOfWeek = getDay(d);
 
-        lastUpdated = d
-        const formattedDate = format(d, 'EEEE do MMMM', defaultTimezone) || 'Today';
-        bot.postMessageToGroup('daily_update', `Daily update for ${formattedDate}`, params);
-      }
+
+    if ((h === 9 && m === 01) && dayOfWeek !== 6 && dayOfWeek !== 7) {
+    if (lastUpdated !== dayOfYear + h + m) {
+      lastUpdated = dayOfYear + h + m;
+      const quote = getRandomArrayItem(quotes)
+      const formattedDate = format(d, 'EEEE do MMMM', defaultTimezone) || 'Today';
+      const author = !!quote?.author ? quote.author : 'Unknown'
+      var params = {
+        icon_emoji: ':mega:',
+        blocks: [{
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": `<!here> *Daily update for ${formattedDate}* \n\n`
+            },
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": `*${quote.text}* - ${author}`
+            }
+          }
+        ]
+      };
+      bot.postMessageToGroup('daily_update', null, params);
+      log(chalk.green(`Message posted at ${h}:${m}:${s}`))
+    }
     }
   }
 
   setInterval(() => {
     pollDate()
-  }, 1000)
-
+  }, 30000)
 });
